79b341c3db9c059e9152123d8a14ac0835d26b12
mattvchandler <tardarsauce@gmail.com>
2018-03-21 10:51:01 -0800

    add test quad

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,5 +1,9 @@
 #include <mutex>
 #include <thread>
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <memory>
 
 #include <android/native_window.h>
 #include <android/native_window_jni.h>
@@ -10,10 +14,142 @@
 #include <EGL/eglext.h>
 #include <GLES3/gl3.h>
 
+class Shader_prog
+{
+private:
+    std::unordered_map<std::string, GLint> uniforms;
+    GLuint id;
+public:
+
+    Shader_prog(const std::vector<std::pair<std::string, GLenum>> sources)
+    {
+        std::vector<Shader_obj> shaders;
+        for(auto &source: sources)
+            shaders.emplace_back(source.first, source.second);
+
+        id = glCreateProgram();
+
+        for(auto & s: shaders)
+            glAttachShader(id, s.get_id());
+
+        glLinkProgram(id);
+
+        GLint link_status;
+        glGetProgramiv(id, GL_LINK_STATUS, &link_status);
+        if(link_status != GL_TRUE)
+        {
+            GLint log_length;
+            glGetProgramiv(id, GL_INFO_LOG_LENGTH, &log_length);
+            std::vector<char> log(static_cast<std::size_t>(log_length + 1), '\0');
+            glGetProgramInfoLog(id, log_length, NULL, log.data());
+
+            glDeleteProgram(id);
+            id = 0;
+
+            __android_log_print(ANDROID_LOG_ERROR, "Shader_prog::Shader_proj", "Error linking shader program:\n %s", log.data());
+            throw std::system_error(link_status, std::system_category(), "Error linking shader program:\n" + std::string(log.data()));
+        }
+
+        // get uniforms
+        GLint num_uniforms;
+        GLint max_buff_size;
+        glGetProgramiv(id, GL_ACTIVE_UNIFORMS, &num_uniforms);
+        glGetProgramiv(id, GL_ACTIVE_UNIFORM_MAX_LENGTH, &max_buff_size);
+
+        std::vector<char> uniform(static_cast<std::size_t>(max_buff_size + 1), '\0');
+
+        for(GLuint i = 0; i < static_cast<GLuint>(num_uniforms); ++i)
+        {
+            GLint size; GLenum type;
+            glGetActiveUniform(id, i, static_cast<GLint>(uniform.size()), NULL, &size, &type, uniform.data());
+
+            GLint loc = glGetUniformLocation(id, uniform.data());
+            if(loc != -1)
+                uniforms[uniform.data()] = loc;
+        }
+    }
+    ~Shader_prog()
+    {
+        if(id)
+            glDeleteProgram(id);
+    }
+    Shader_prog(const Shader_prog &) = delete;
+    Shader_prog & operator=(const Shader_prog &) = delete;
+    Shader_prog(Shader_prog && other): id(other.id) { other.id = 0; };
+    Shader_prog & operator=(Shader_prog && other)
+    {
+        if(this != &other)
+        {
+            id = other.id;
+            other.id = 0;
+        }
+        return *this;
+    }
+
+    void use() const { glUseProgram(id); }
+    GLint get_uniform(const std::string & uniform) const { return uniforms.at(uniform); }
+
+    class Shader_obj
+    {
+    private:
+        GLuint id;
+    public:
+        Shader_obj(const std::string & src, GLenum type)
+        {
+            id  = glCreateShader(type);
+
+            const char * data = src.c_str();
+            glShaderSource(id, 1, &data, NULL);
+            glCompileShader(id);
+
+            GLint compile_status;
+            glGetShaderiv(id, GL_COMPILE_STATUS, &compile_status);
+
+            if(compile_status != GL_TRUE)
+            {
+                GLint log_length;
+                glGetShaderiv(id, GL_INFO_LOG_LENGTH, &log_length);
+                std::vector<char> log(static_cast<std::size_t>(log_length), '\0');
+                glGetShaderInfoLog(id, log_length, NULL, log.data());
+
+                glDeleteShader(id);
+                id = 0;
+
+                __android_log_print(ANDROID_LOG_ERROR, "Shader_prog::Shader_obj::Shader_obj", "Error compiling shader:\n %s", log.data());
+                throw std::system_error(compile_status, std::system_category(), "Error compiling shader:\n" + std::string(log.data()));
+            }
+        }
+        ~Shader_obj()
+        {
+            if(id)
+                glDeleteShader(id);
+        }
+        Shader_obj(const Shader_obj &) = delete;
+        Shader_obj & operator=(const Shader_obj &) = delete;
+        Shader_obj(Shader_obj && other): id(other.id) { other.id = 0; };
+        Shader_obj & operator=(Shader_obj && other)
+        {
+            if(this != &other)
+            {
+                id = other.id;
+                other.id = 0;
+            }
+            return *this;
+        }
+
+        GLuint get_id() const { return id; }
+    };
+};
+
+
 ANativeWindow * window = nullptr;
 EGLDisplay display = EGL_NO_DISPLAY;
 EGLSurface surface = EGL_NO_SURFACE;
 EGLContext context = EGL_NO_CONTEXT;
+
+GLuint vao = 0;
+GLuint vbo = 0;
+std::unique_ptr<Shader_prog> prog;
 
 std::thread render_thread;
 std::mutex render_mutex;
@@ -21,6 +157,14 @@ bool running = false;
 
 void destroy()
 {
+    glDeleteBuffers(1, &vbo);
+    glDeleteVertexArrays(1, &vao);
+    prog.reset();
+
+    vao = 0;
+    vbo = 0;
+    prog = 0;
+
     eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
     eglDestroyContext(display, context);
     eglDestroySurface(display, surface);
@@ -35,25 +179,50 @@ const char* eglGetErrorString( EGLint error )
 {
     switch( error )
     {
-    CASE_STR( EGL_SUCCESS             )
-    CASE_STR( EGL_NOT_INITIALIZED     )
-    CASE_STR( EGL_BAD_ACCESS          )
-    CASE_STR( EGL_BAD_ALLOC           )
-    CASE_STR( EGL_BAD_ATTRIBUTE       )
-    CASE_STR( EGL_BAD_CONTEXT         )
-    CASE_STR( EGL_BAD_CONFIG          )
-    CASE_STR( EGL_BAD_CURRENT_SURFACE )
-    CASE_STR( EGL_BAD_DISPLAY         )
-    CASE_STR( EGL_BAD_SURFACE         )
-    CASE_STR( EGL_BAD_MATCH           )
-    CASE_STR( EGL_BAD_PARAMETER       )
-    CASE_STR( EGL_BAD_NATIVE_PIXMAP   )
-    CASE_STR( EGL_BAD_NATIVE_WINDOW   )
-    CASE_STR( EGL_CONTEXT_LOST        )
+        CASE_STR( EGL_SUCCESS             )
+        CASE_STR( EGL_NOT_INITIALIZED     )
+        CASE_STR( EGL_BAD_ACCESS          )
+        CASE_STR( EGL_BAD_ALLOC           )
+        CASE_STR( EGL_BAD_ATTRIBUTE       )
+        CASE_STR( EGL_BAD_CONTEXT         )
+        CASE_STR( EGL_BAD_CONFIG          )
+        CASE_STR( EGL_BAD_CURRENT_SURFACE )
+        CASE_STR( EGL_BAD_DISPLAY         )
+        CASE_STR( EGL_BAD_SURFACE         )
+        CASE_STR( EGL_BAD_MATCH           )
+        CASE_STR( EGL_BAD_PARAMETER       )
+        CASE_STR( EGL_BAD_NATIVE_PIXMAP   )
+        CASE_STR( EGL_BAD_NATIVE_WINDOW   )
+        CASE_STR( EGL_CONTEXT_LOST        )
+        default: return "Unknown";
+    }
+}
+const char* glGetErrorString(GLenum error )
+{
+    switch( error )
+    {
+    CASE_STR(GL_NO_ERROR)
+    CASE_STR(GL_INVALID_ENUM)
+    CASE_STR(GL_INVALID_VALUE)
+    CASE_STR(GL_INVALID_OPERATION)
+    CASE_STR(GL_INVALID_FRAMEBUFFER_OPERATION)
+    CASE_STR(GL_OUT_OF_MEMORY)
     default: return "Unknown";
     }
 }
 #undef CASE_STR
+
+void GL_check_error(const char * at)
+{
+    GLenum e = glGetError();
+    if(e != GL_NO_ERROR)
+    {
+        using namespace std::string_literals;
+        __android_log_print(ANDROID_LOG_ERROR, "Error at %s: %s", at, glGetErrorString(e));
+        throw std::system_error(e, std::system_category(), "Error at "s + std::string(at) + ":\n"s + glGetErrorString(e));
+    }
+}
+
 bool init()
 {
     __android_log_write(ANDROID_LOG_DEBUG, "INIT", "init start");
@@ -106,7 +275,7 @@ bool init()
     ANativeWindow_setBuffersGeometry(window, 0, 0, native_visual_id);
 
     surface = eglCreateWindowSurface(display, config, window, NULL);
-    if (surface == EGL_NO_SURFACE)
+    if(surface == EGL_NO_SURFACE)
     {
         __android_log_print(ANDROID_LOG_ERROR, "INIT", "eglCreateWindowSurface error: %s", eglGetErrorString(eglGetError()));
         destroy();
@@ -122,7 +291,7 @@ bool init()
         return false;
     }
 
-    if (!eglMakeCurrent(display, surface, surface, context))
+    if(!eglMakeCurrent(display, surface, surface, context))
     {
         __android_log_print(ANDROID_LOG_ERROR, "INIT", "eglMakeCurrent error: %s", eglGetErrorString(eglGetError()));
         destroy();
@@ -130,14 +299,56 @@ bool init()
     }
 
     EGLint width, height;
-    if (!eglQuerySurface(display, surface, EGL_WIDTH, &width) || !eglQuerySurface(display, surface, EGL_HEIGHT, &height))
+    if(!eglQuerySurface(display, surface, EGL_WIDTH, &width) || !eglQuerySurface(display, surface, EGL_HEIGHT, &height))
     {
         __android_log_print(ANDROID_LOG_ERROR, "INIT", "eglQuerySurface error: %s", eglGetErrorString(eglGetError()));
         destroy();
         return false;
     }
 
+    __android_log_write(ANDROID_LOG_DEBUG, "DEBUG", "EGL set up.");
+
+    GLfloat verts [] =
+    {
+        -0.5f, -0.5f,
+        -0.5f,  0.5f,
+         0.5f, -0.5f,
+         0.5f,  0.5f
+    };
+
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    glEnableVertexAttribArray(0);
+    GL_check_error("vao/vbo");
+
+    const char * vertshader =
+ R"(#version 300 es
+    layout(location = 0) in vec2 vert_pos;
+    void main()
+    {
+        gl_Position = vec4(vert_pos, 0.0, 1.0);
+    }
+    )";
+    const char * fragshader =
+ R"(#version 300 es
+    precision mediump float;
+
+    out vec4 frag_color;
+    void main()
+    {
+        frag_color = vec4(0.0, 1.0, 0.0, 1.0);
+    }
+    )";
+
+    prog = std::make_unique<Shader_prog>(std::vector<std::pair<std::string, GLenum>>{std::make_pair(std::string(vertshader), GL_VERTEX_SHADER), std::make_pair(std::string(fragshader), GL_FRAGMENT_SHADER)});
+
     glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    glViewport(0, 0, width, height);
 
     __android_log_write(ANDROID_LOG_DEBUG, "DEBUG", "init success");
     return true;
@@ -181,6 +392,13 @@ void render_loop()
 
         glClearColor(r, g, b, 0.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        prog->use();
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+        GL_check_error("draw");
+
         if(!eglSwapBuffers(display, surface))
         {
             __android_log_write(ANDROID_LOG_ERROR, "RENDER_LOOP", "couldn't swap!");
